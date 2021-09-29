cc5750848775cac6ec79d7246cf50d81fb479370
Brenaki <91611155+Brenaki@users.noreply.github.com>
2021-09-29 11:13:56 -0300

    Dados da Lojinha
    Add todos os produtos que a lojinha vende

---
+++ Dados da Lojinha do Zé.sql
@@ -0,0 +1,14 @@
+select * from Produtos;
+
+INSERT INTO `LojinhaDoZe`.`Produtos` (`Nome`, `Descrição`, `Preço_un`, `Quantidade_Estoque`) VALUES ('Água Mineral Bonafont', 'Água Mineral Natural com Gás Bonafont Água Leve Garrafa 500ml', '1.79', '48');
+INSERT INTO `LojinhaDoZe`.`Produtos` (`Nome`, `Descrição`, `Preço_un`, `Quantidade_Estoque`) VALUES ('Ketchup Heinz', 'Ketchup Heinz Tradicional 397g', '11', '15');
+INSERT INTO `LojinhaDoZe`.`Produtos` (`Nome`, `Descrição`, `Preço_un`, `Quantidade_Estoque`) VALUES ('Maionese Heinz', 'Maionese Heinz Tradicional 400g', '13', '23');
+INSERT INTO `LojinhaDoZe`.`Produtos` (`Nome`, `Descrição`, `Preço_un`, `Quantidade_Estoque`) VALUES ('Bala De Yogurte', 'Bala De Yogurte 100un Original Dori', '9', '13');
+INSERT INTO `LojinhaDoZe`.`Produtos` (`Nome`, `Descrição`, `Preço_un`, `Quantidade_Estoque`) VALUES ('Vodka Absolut', 'Vodka Absolut Natural 1L', '100', '4');
+INSERT INTO `LojinhaDoZe`.`Produtos` (`Nome`, `Descrição`, `Preço_un`, `Quantidade_Estoque`) VALUES ('Gin London Dry', 'Gin London Dry 750ml Tanqueray', '110', '12');
+INSERT INTO `LojinhaDoZe`.`Produtos` (`Nome`, `Descrição`, `Preço_un`, `Quantidade_Estoque`) VALUES ('Cerveja Heineken', 'Cerveja Heineken Lata 350ml', '4', '180');
+INSERT INTO `LojinhaDoZe`.`Produtos` (`Nome`, `Descrição`, `Preço_un`, `Quantidade_Estoque`) VALUES ('Bolacha Bono', 'Bono Bisc Recheado Chocolate 126g ', '2', '30');
+INSERT INTO `LojinhaDoZe`.`Produtos` (`Nome`, `Descrição`, `Preço_un`, `Quantidade_Estoque`) VALUES ('Cookie Garoto', 'Cookie Garoto Gotas De Chocolate 60g', '3', '20');
+INSERT INTO `LojinhaDoZe`.`Produtos` (`Nome`, `Descrição`, `Preço_un`, `Quantidade_Estoque`) VALUES ('Bolacha Passatempo', 'Biscoito Passatempo Leite 150g', '2', '15');
+INSERT INTO `LojinhaDoZe`.`Produtos` (`Nome`, `Descrição`, `Preço_un`, `Quantidade_Estoque`) VALUES ('Chicle Trident', 'Chicle Trident Mondelez Tutti Frutti', '1', '58');
+INSERT INTO `LojinhaDoZe`.`Produtos` (`Nome`, `Descrição`, `Preço_un`, `Quantidade_Estoque`) VALUES ('Energético Red Bull', 'Energético Red Bull Energy Drink Lata 250ml', '8', '19');
